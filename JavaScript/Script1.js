d888692e825e4d8eb9e277885bf7fc9d94eda174
buccatm <70668682+buccatm@users.noreply.github.com>
2022-11-15 19:41:10 -1000

    Added a Table
    Added a table to display genre with total

--- JavaScript/Script1.js
+++ JavaScript/Script1.js
@@ -1,4 +1,5 @@
 TESTER = document.getElementById('myDiv');
+TABLE = document.getElementById('myTable');
 const inputFile = document.querySelector('#file');
 const t = []; // holds the total for each genres
 const dummy = []; // temp holder
@@ -8,60 +9,87 @@ inputFile.addEventListener("change", async () => {
     const excelFile = inputFile.files[0]
     // Do all the work in here
     dfd.readExcel(excelFile).then((df) => {
-       const tittle = df.name.getColumnData;
-       const genre = df.genres.getColumnData;
-       const total = [];
-       console.log(genre);
+        const tittle = df.name.getColumnData;
+        const genre = df.genres.getColumnData;
+        const total = [];
+        console.log(genre);
 
-       // handles spliting games with multiple genres
-       for(let i = 0; i < genre.length; i++) {
+        // handles spliting games with multiple genres
+        for (let i = 0; i < genre.length; i++) {
             const temp = genre[i].split(';'); // splits ["genreA;genreB"] --> ['genreA', 'genreB']
             if (temp.length > 1) { // if has multiple gerne
-                for(let x = 0; x < temp.length; x++) {
+                for (let x = 0; x < temp.length; x++) {
                     dummy.push(temp[x]); // push each individually into dummy
                 }
             } else { // if there is only one genre
                 dummy.push(temp.toString());
             }
-       }
-       const uniqGenres = new Set(dummy); // gets the uniq genres, total 29 uniq genres
-       
+        }
+        const uniqGenres = new Set(dummy); // gets the uniq genres, total 29 uniq genres
 
-       uniqGenres.forEach(doWork); // totals up each genres
-       const x = [];
-       const y = [];
-       for (let a = 0 ; a < t.length; a++) {
+
+        uniqGenres.forEach(doWork); // totals up each genres
+        const x = [];
+        const y = [];
+        for (let a = 0; a < t.length; a++) {
             x.push(Object.keys(t[a]).toString());
             y.push(Object.values(t[a])[0]);
-       }
-       console.log(x);
-       console.log(y);
+        }
+        console.log(x);
+        console.log(y);
+        const tableVal = [
+            x, y
+        ];
 
-       const trace1 = {
+        const trace1 = {
             x: x,
             y: y,
             type: 'bar',
-       };
+        };
 
-       const layout = {
-        title: 'Marker Size',
-        showlegend: false,
-        height: 900,
-        width: 1000
-      };
+        const table1 = {
+            type: 'table',
+            header: {
+                values: ['Genre', 'Total'],
+                align: "center",
+                line: { width: 1, color: 'black' },
+                fill: { color: "grey" },
+                font: { family: "Arial", size: 12, color: "white" }
+            },
+            cells: {
+                values: tableVal
+            }
+        };
 
-       const data = [trace1];
+
+
+        const layout = {
+            title: 'Steam Games Data',
+            showlegend: false,
+            height: 900,
+            width: 1000
+        };
+
+        const table_layout = {
+            title: 'Steam Games Data',
+            height: 900,
+            width: 1000
+        };
+
+        const data = [trace1];
+        const tableData = [table1];
         Plotly.newPlot('myDiv', data, layout);
+        Plotly.newPlot(TABLE, tableData, table_layout);
     })
 })
 
 
 const doWork = (value) => {
-   const temp = dummy.filter(item => item === value); // find the all genre === value from dummy
-   const name = temp[0]; // gets the name of that genre
-   const data = {}; // temp holder
-   data[name] = temp.length; // asigns data = { 'genreName' : total number of genre}
-   t.push(data);
+    const temp = dummy.filter(item => item === value); // find the all genre === value from dummy
+    const name = temp[0]; // gets the name of that genre
+    const data = {}; // temp holder
+    data[name] = temp.length; // asigns data = { 'genreName' : total number of genre}
+    t.push(data);
 };
 
-  
+

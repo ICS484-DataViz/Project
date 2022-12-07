c14a590650c355570b91a0be1c705fcfee0bfbf3
Chad Oshiro <oshiroc3@hawaii.edu>
2022-12-06 23:40:05 -1000

    finished second graph

--- JavaScript/Script3.js
+++ JavaScript/Script3.js
@@ -7,10 +7,21 @@ inputFileMac.addEventListener("change", async () => {
   const excelFile = inputFileMac.files[0];
 
   dfd.readCSV(excelFile).then((df) => {
+    console.log(df);
     const tempDate = [];
     const games = df.name.getColumnData;
     const releaseDate = df.release_date.getColumnData;
     const platform = df.platforms.getColumnData;
+    const avg = df.average_playtime.getColumnData;
+    console.log(avg);
+
+    releaseDate.forEach(function (dates) {
+      tempDate.push(new Date(dates).getFullYear());
+    })
+    const getYearsOnly = tempDate;
+    const tempData = games.map((element, index) => {
+      return [games[index], avg[index], getYearsOnly[index]];
+    });
 
     // grabs the years from the date column of the csv file
     for (let i = 0; i < releaseDate.length; i++) {
@@ -41,6 +52,28 @@ inputFileMac.addEventListener("change", async () => {
       macY.push(Object.values(macTotal[i])[0]);
     }
 
+    tempData.forEach(function (elements) {
+      const temp = {};
+      Object.assign(temp, elements);
+      temp['title'] = temp[0];
+      temp['AveragePlaytime'] = temp[1];
+      temp['YearOnly'] = temp[2];
+      delete temp[0];
+      delete temp[1];
+      delete temp[2];
+
+      data.push(temp);
+    });
+
+    const avgPlayData = data.sort(sortAveragePlayTime);
+    const highestPlayTime = avgPlayData.filter((items) => ((items.AveragePlaytime / 100) >= 100));
+
+    console.log("Most PlayTime: ", highestPlayTime);
+    const topGames = highestPlayTime.slice(0, 10);
+
+    const nameOfGame = topGames.map((items) => items.title);
+    const averagePlaytime = topGames.map((items) => items.AveragePlaytime);
+
     const macTrace = [{
       x: macX,
       y: macY,
@@ -61,8 +94,31 @@ inputFileMac.addEventListener("change", async () => {
       width: 1000
     }
 
+    const averageTrace = [
+      {
+        x: nameOfGame,
+        y: averagePlaytime,
+        type: 'bar',
+        name: 'Average Playtime'
+      }
+    ];
+
+    const averageLayout = {
+      title: 'Top 10 Games with the Highest Average Playtime',
+      showlegend: true,
+      height: 1100,
+      width: 900,
+      yaxis: {
+        title: 'Average Playtime in Minutes',
+      },
+      xaxis: {
+        title: 'Top 10 Games',
+        tickangle: -45,
+        text: 'Games'
+    }}
 
     Plotly.newPlot('MacGames', macTrace, macLayout);
+    Plotly.newPlot('AveragePlay', averageTrace, averageLayout);
   })
 
 });
@@ -75,3 +131,6 @@ const doWorkMac = (value) => {
   macTotal.push(data);
 };
 
+const sortAveragePlayTime = (a, b) => {
+  return b.AveragePlaytime - a.AveragePlaytime;
+};
